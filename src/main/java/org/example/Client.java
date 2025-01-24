17e3560c7f93911fe352b61f7d14ed487ca604c0
AKSUM <51446624+AKSUMRUS@users.noreply.github.com>
2025-01-24 11:31:32 +0300

    init commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Random;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        String ip = args[0];
+        int port = Integer.parseInt(args[1]);
+        int N = Integer.parseInt(args[2]);
+        int M = Integer.parseInt(args[3]);
+        int Q = Integer.parseInt(args[4]);
+
+        try (Socket s = new Socket(ip, port);
+                FileWriter fileWriter = new FileWriter("result.txt");) {
+            s.setTcpNoDelay(true); // TCP_NO_DELAY = true
+            OutputStream writer = s.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            for (int K = 0; K < M; K++) {
+                int len = N * K + 8;
+                double time = 0;
+                for (int i = 0; i < Q; i++) {
+                    byte[] array = new byte[len];
+                    new Random().nextBytes(array);
+                    long before = System.currentTimeMillis();
+                    writer.write(array);
+                    writer.flush();
+
+                    reader.readLine();
+                    long after = System.currentTimeMillis();
+                    time += after - before;
+                }
+                time /= Q;
+
+                System.out.printf("Итерация #%d\n", K);
+                System.out.printf("Количество отправленных байт: %d\n", len);
+                System.out.printf("Среднее время: %f\n", time);
+                System.out.println("-------------");
+                fileWriter.write(String.format("%d %f\n", len, time));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket ss = new ServerSocket(10000)) {
+            while (true) {
+                Socket s = ss.accept();
+
+                new Thread(() -> handle(s)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void handle(Socket s) {
+        try {
+            InputStream reader = s.getInputStream();
+            byte[] buffer = new byte[4096];
+            PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
+            while (s.isConnected()) {
+                int n = 0;
+
+                while (reader.available() > 0 && (n = reader.read(buffer)) != -1) {}
+
+                if(n > 0) {
+                    String timeStamp = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date());
+                    writer.println(timeStamp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
